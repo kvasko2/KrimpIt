a70e1bf70756c9c6f77be68225f19007d3fd244b
kvasko2 <kvasko2@gmail.com>
2018-02-14 19:06:54 -0500

    Body Joint position basics.

---
+++ Assets/Scripts/DetectJoints.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class DetectJoints : MonoBehaviour {
+
+	public GameObject bodyManager;
+	public JointType trackedJoint;
+	public float distMultiplier = 1f;
+	private BodySourceManager bodyMgr;
+	private Body[] bodies;
+
+	// Use this for initialization
+	void Start () {
+		if (bodyManager == null) {
+			Debug.Log ("Assign body manager.");
+		} else {
+			bodyMgr = bodyManager.GetComponent<BodySourceManager> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bodyMgr != null) {
+			bodies = bodyMgr.GetData ();
+
+			if (bodies != null) {
+				foreach (Body body in bodies) {
+					if (body != null) {
+						if (body.IsTracked) {
+							CameraSpacePoint pos = body.Joints [trackedJoint].Position;
+							gameObject.transform.position = new Vector3 (pos.X * distMultiplier, pos.Y * distMultiplier, 0f);
+						}
+					}
+				}
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # KrimpIt
+
+## Helpful resources
+0. [Unity Kinect V2 package](http://go.microsoft.com/fwlink/?LinkId=513177)
+0. [Video for getting finding Kinect body joints](https://channel9.msdn.com/Blogs/2p-start/Gesture-Control-with-Kinect-and-Unity-made-easy)
